cfb185ebf307ceaf1c1053921ad7de397205e088
Mikiyas Olika <mikiyasolika@gmail.com>
2019-01-31 22:41:57 +0300

    dummy data added

---
+++ database.js
@@ -0,0 +1,22 @@
+export default data = [
+    {
+        "name": "Mikiyas Olika",
+        "age":  "22",
+        "time": "12:55"
+    },
+    {
+        "name": "Mikiyas Olika",
+        "age":  "22",
+        "time": "12:55"
+    },
+    {
+        "name": "Mikiyas Olika",
+        "age":  "22",
+        "time": "12:55"
+    },
+    {
+        "name": "Mikiyas Olika",
+        "age":  "22",
+        "time": "12:55"
+    }
+]
